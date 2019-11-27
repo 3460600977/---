ab27697675a47b589c03d5dcef42bb2629c29127
何彬 <hebin@xinchao.com>
2019-11-27 14:41:45 +0800

    多环境打包

--- src/api/modules/test.js
+++ src/api/modules/test.js
@@ -1,6 +1,7 @@
 import request from "@/utils/request";
 
 export function test(data) {
+  console.log(process.env.NODE_ENV)
   return request({
     url: "http://qa-effect.xinchao.com/trends/campaigns?keyword=&pageNum=&pageSize=",
     method: "get",
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import {putManageRouter} from './modules/putManage' // 投放管理
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/',
