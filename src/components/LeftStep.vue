153b62ce2c72ea272b4a7f1d1d7f97d671a689cb
chenxu <chenxu@xinchao.com>
2019-12-01 14:16:02 +0800

    '添加投放管理流程文件以及路由'

--- src/components/LeftStep.vue
+++ src/components/LeftStep.vue
@@ -1,5 +1,11 @@
 <template>
-    $END$
+  <div style="height: 300px;">
+    <el-steps direction="vertical" :active="1">
+      <el-step title="步骤 1"></el-step>
+      <el-step title="步骤 2"></el-step>
+      <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
+    </el-steps>
+  </div>
 </template>
 
 <script>
--- src/views/putManage/flow/CreatePlan.vue
+++ src/views/putManage/flow/CreatePlan.vue
@@ -1,13 +1,38 @@
 <template>
-    $END$
+  <div style="height: 300px;">
+    <el-steps direction="vertical" :active="1">
+      <el-step title="步骤 1">
+        <div slot="description">
+          <div v-for="step1 in step1_des">{{ step1 }}</div>
+        </div>
+      </el-step>
+      <el-step title="步骤 2">
+        <div slot="description">
+          <div v-for="step2 in step2_des">{{ step2 }}</div>
+        </div>
+      </el-step>
+      <el-step title="步骤 3" style=" white-space: pre-line;">
+        <div slot="description">
+          <div v-for="step3 in step3_des">{{ step3 }}</div>
+        </div>
+      </el-step>
+    </el-steps>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "CreatePlan"
+        name: "CreatePlan",
+        data() {
+            return {
+                step1_des: ['投放目的', '投放计划名称'],
+                step2_des: ['投放设置', '楼盘定向', '投放方案名称'],
+                step3_des: ['制作创意', '创意资质', '广告创意名称'],
+            };
+        },
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
