3c7b202ed61fce0a7062616d3aff163b877d261c
何彬 <hebin@xinchao.com>
2019-12-03 10:08:52 +0800

    调整投放管理目录结构

--- src/router/modules/putManage.js
+++ src/router/modules/putManage.js
@@ -1,20 +1,23 @@
-// 投放管理
+/**
+ * 投放管理
+ */ 
 export const putManageRouter = [
-  // 投放管理-投放计划
+  // 投放计划列表
   {
     path: '/plan/list',
     component: () => import ('@/views/putManage/list/modules/plan/List'),
   },
-  // 投放管理-投放方案
+  // 投放方案列表
   {
     path: '/project/list',
     component: () => import ('@/views/putManage/list/modules/project/List'),
   },
-  // 投放管理-广告创意
+  // 广告创意列表
   {
     path: '/creative/list',
     component: () => import ('@/views/putManage/list/modules/creative/List'),
   },
+  // 创建流程
   {
     path: '/putManage',
     component: () => import ('@/views/putManage/Index'),
