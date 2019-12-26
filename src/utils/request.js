2e018c805d5394c4f7ce0153679a6a30aa83232a
chenxu <chenxu@xinchao.com>
2019-12-26 17:38:18 +0800

    '最新登录接口'

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,13 +15,13 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // Do something before request is sent
-    // if (store.state.token.userToken) {
-    //   // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-    //   config.headers['token'] = store.state.token.userToken;
-    // } else {
-    //   config.headers['token'] = '';
-    // }
-    config.headers['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJrYWtheGkiLCJleHAiOjE1NzcwODkyODh9.wNNEK6JAxcBHgsKpRwDJ46-NR1z0SvChOXj1ERcJdRo';
+    if (store.state.token.userToken) {
+      // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+      config.headers['token'] = store.state.token.userToken;
+    } else {
+      config.headers['token'] = '';
+    }
+    //config.headers['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJrYWtheGkiLCJleHAiOjE1NzcwODkyODh9.wNNEK6JAxcBHgsKpRwDJ46-NR1z0SvChOXj1ERcJdRo\n';
     return config
   },
   error => {
