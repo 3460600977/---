b4410e31736c0e8e20de1eda623c2fff14621e9d
jiangyao <jiangyao360@126.com>
2019-12-31 20:32:08 +0800

    create

--- src/views/peopleInsight/createPeoplePackage/createForm.vue
+++ src/views/peopleInsight/createPeoplePackage/createForm.vue
@@ -5,10 +5,12 @@
         <el-input v-model="form.name"></el-input>
       </el-form-item>
       <el-form-item label="人群包描述：" style="margin-bottom: 30px">
-        <el-input type="textarea" v-model="form.text"></el-input>
+        <el-input type="textarea" v-model="form.description"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="cancle('ruleForm')">取消</el-button>
+        <router-link to="/peopleInsight">
+          <el-button @click="">取消</el-button>
+        </router-link>
         <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
       </el-form-item>
     </el-form>
@@ -16,6 +18,8 @@
 </template>
 
 <script>
+  import {mapState} from "vuex";
+
   export default {
     name: "createForm",
     data() {
@@ -27,22 +31,63 @@
         },
         form: {
           name: '',
-          text: ''
+          description: '',
+          tags: '',
+          tagsName: '',
+          city: ''
         },
       }
     },
     methods: {
-      cancle() {},
       submitForm(form) {
         this.$refs[form].validate((valid) => {
           if (valid) {
-            alert('submit!');
+            if (this.crowdProject.city === ""){
+              alert('城市必选!');
+            }
+            //拼接数据
+            this.form.tags = this.crowdProject.tagTid.join(",");
+            this.form.tagsName = JSON.stringify(this.crowdProject.tagNames)
+            this.form.city = this.crowdProject.city;
+            const h = this.$createElement;
+            this.$msgbox({
+              title: '是否创建人群包？',
+              message: h('p', null, [
+                h('span', null, '生成人群需要一定的时间，请您耐心等待！'),
+                h('span', null, '生成100万以下的人群在'),
+                h('i', { style: 'color: red' }, '2个小时'),
+                h('span', null, '之内完成； 生成100万以上的人群至少需要'),
+                h('i', { style: 'color: red' }, '8个小时'),
+                h('span', null, '完成；'),
+                h('span', null, '生成完后，可以在“媒体智选”页面，利用人群包进行选点'),
+              ]),
+              showCancelButton: true,
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+            }).then(() => {
+              this.$api.peopleInsight.createPeopleCrowd(this.form)
+                .then(res => {
+                  //跳转至列表页
+                  this.$router.push('/peopleInsight')
+                })
+                .catch(res => {
+                  alert('保存失败!!');
+                })
+            }).catch(action=>{
+              this.$message({
+                type: 'info',
+                message: 'action:'+action
+              })
+            });
           } else {
-            console.log('error submit!!');
+            alert('保存失败!');
             return false;
           }
         });
       },
+    },
+    computed:{
+      ...mapState(["crowdProject"])
     }
   }
 </script>
