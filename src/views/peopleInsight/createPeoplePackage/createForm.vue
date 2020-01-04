f1de12830770f7e4bc8034d799f4698942598048
jiangyao <jiangyao360@126.com>
2020-01-04 18:53:43 +0800

    dddd

--- src/views/peopleInsight/createPeoplePackage/createForm.vue
+++ src/views/peopleInsight/createPeoplePackage/createForm.vue
@@ -12,6 +12,19 @@
         <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
       </el-form-item>
     </el-form>
+    <el-dialog title="是否创建人群包？"
+               :visible.sync="dialogShowContent"
+               width="568px"
+               class="my-dialog"
+    >
+      <p>生成人群需要一定的时间，请您耐心等待</p>
+      <p>生成100万以下的人群在<span class="red-text">2个小时</span>之内完成；生成100万以上的人群至少需要<span class="red-text">8个小时</span>完成；</p>
+      <p>1千以下的人群，无法生成人群画像。生成完成后，可以在"媒体直选"页面，利用人群包进行选点</p>
+      <span slot="footer">
+        <el-button @click="dialogShowContent = false" class="btn1">取 消</el-button>
+        <el-button type="primary" class="btn1" @click="complete">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -34,6 +47,7 @@
           tagsName: '',
           city: ''
         },
+        dialogShowContent: false,
       }
     },
     methods: {
@@ -41,74 +55,61 @@
       submitForm(form) {
         this.$refs[form].validate((valid) => {
           if (valid) {
-            //拼接数据
-            //1 拼接tid  用于后台调用酷云
-            //2 拼接tagnames  用于人群分析的展示
-            //3 拼接city  分别在列表页以及人群分析展示(城市的名称)
-            let city = "";
-            let childTids = "";
-            this.crowdProject.tagNames.forEach((tags,index)=>{
-              if (tags.tid === this.crowdProject.cityTid) {
-                city = tags.tags[0].name;
-              }
-              childTids += "(";
-                tags.tags.forEach((tagInfo,tagIndex)=>{
-                childTids += tagInfo.tid;
-                if (tagIndex < tags.tags.length-1) {
-                  childTids += "|";
-                }
-              });
-              childTids += ")";
-              if (index < this.crowdProject.tagNames.length-1){
-                childTids += ",";
-              }
-            });
-
-            if (city === ""){
-              this.$message({
-                type: 'warning',
-                message: '城市必选'
-              });
-              return false;
-            }
-            this.form.tags = childTids;
-            this.form.tagsName = JSON.stringify(this.crowdProject.tagNames);
-            this.form.city = city;
-            const h = this.$createElement;
-            this.$msgbox({
-              title: '是否创建人群包？',
-              message: h('p', null, [
-                h('span', null, '生成人群需要一定的时间，请您耐心等待！'),
-                h('span', null, '生成100万以下的人群在'),
-                h('i', { style: 'color: red' }, '2个小时'),
-                h('span', null, '之内完成； 生成100万以上的人群至少需要'),
-                h('i', { style: 'color: red' }, '8个小时'),
-                h('span', null, '完成；'),
-                h('span', null, '生成完后，可以在“媒体智选”页面，利用人群包进行选点'),
-              ]),
-              showCancelButton: true,
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-            }).then(() => {
-              this.$api.peopleInsight.createPeopleCrowd(this.form)
-                .then(res => {
-                  this.removeAllState();
-                  //跳转至列表页
-                  this.$router.push('/peopleInsight/list');
-                })
-                .catch(res => {
-                  this.$message({
-                    type: 'info',
-                    message: '保存失败'
-                  })
-                })
-            }).catch(action=>{
-
-            });
+            this.dialogShowContent = true
           } else {
             return false;
           }
         });
+      },
+      complete(){
+        //拼接数据
+        //1 拼接tid  用于后台调用酷云
+        //2 拼接tagnames  用于人群分析的展示
+        //3 拼接city  分别在列表页以及人群分析展示(城市的名称)
+        //关闭弹窗
+        this.dialogShowContent = false;
+        let city = "";
+        let childTids = "";
+        this.crowdProject.tagNames.forEach((tags,index)=>{
+          if (tags.tid === this.crowdProject.cityTid) {
+            city = tags.tags[0].name;
+          }
+          childTids += "(";
+          tags.tags.forEach((tagInfo,tagIndex)=>{
+            childTids += tagInfo.tid;
+            if (tagIndex < tags.tags.length-1) {
+              childTids += "|";
+            }
+          });
+          childTids += ")";
+          if (index < this.crowdProject.tagNames.length-1){
+            childTids += ",";
+          }
+        });
+
+        if (city === ""){
+          this.$message({
+            type: 'warning',
+            message: '城市必选'
+          });
+          return false;
+        }
+        this.form.tags = childTids;
+        this.form.tagsName = JSON.stringify(this.crowdProject.tagNames);
+        this.form.city = city;
+        console.log(this.form)
+        /*this.$api.peopleInsight.createPeopleCrowd(this.form)
+          .then(res => {
+            this.removeAllState();
+            //跳转至列表页
+            this.$router.push('/peopleInsight/list');
+          })
+          .catch(res => {
+            this.$message({
+              type: 'info',
+              message: '保存失败'
+            })
+          })*/
       },
       backToList(){
         this.removeAllState();
@@ -138,5 +139,8 @@
     .el-button + .el-button {
       margin-left: 60px;
     }
+  }
+  .red-text {
+    color: red;
   }
 </style>
