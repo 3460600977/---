93158e5635109cc12de260535f23e46b3158d1c1
liwen <XinChao@adx119>
2020-01-04 13:02:26 +0800

    详情修改

--- src/components/chart/Histogram.vue
+++ src/components/chart/Histogram.vue
@@ -48,8 +48,18 @@
         }
       },
     },
+    data() {
+      return {
+        myChart: null
+      }
+    },
+    watch: {
+      data(val) {
+        this.reDraw()
+      },
+    },
     mounted() {
-      let myChart = echarts.init(this.$refs.chartBox);
+      this.myChart = echarts.init(this.$refs.chartBox);
       let option = {
         legend: {
           icon: 'circle',
@@ -121,9 +131,84 @@
         },
         series: this.data.yAxis
       };
-      myChart.setOption(option);
+      this.myChart.setOption(option);
     },
     methods: {
+      reDraw() {
+        this.myChart.clear()
+        let option = {
+          legend: {
+            icon: 'circle',
+            show: this.isShowLegend,
+            top: 20,
+            right: 40,
+            padding: 0,
+            itemHeight: 12,
+            textStyle: {
+              color: '#999999'
+            }
+          },
+          tooltip: {
+            trigger: 'axis',
+            formatter: function (params) {
+              let str = `<p>${params[0].name}</p>`
+              params.forEach((item) => {
+                str += `
+                    <p>
+                    <span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;
+                    background:linear-gradient(187deg,${item.color.colorStops[0].color}, ${item.color.colorStops[1].color});"></span>
+                    <span>${item.seriesName}: ${parseInt(item.data * 100)}%</span>
+                    </p>
+                  `
+              })
+              return str
+            }
+          },
+          textStyle: {
+            color: '#999999'
+          },
+          grid: {
+            ...this.grid,
+            containLabel: true
+          },
+          xAxis: {
+            type: 'category',
+            splitLine:{ show:false},
+            axisLine: {show: false},
+            axisTick: {show: false},
+            axisLabel: {
+              margin: 16,
+              interval: 0,
+              rotate: this.rotate
+            },
+            data: this.data.xAxis,
+          },
+          color: this.color,
+          yAxis: {
+            type: 'value',
+            axisLabel: {
+              formatter: function (v) {
+                return `${parseInt(v * 100)}%`
+              }
+            },
+            splitLine:{
+              show: this.isShowLine,
+              lineStyle: {
+                color: '#E5E7E9'
+              },
+            },
+            axisLine: {
+              show: this.isShowLine,
+              lineStyle: {
+                color: '#E5E7E9'
+              },
+            },
+            axisTick: {show: false}
+          },
+          series: this.data.yAxis
+        };
+        this.myChart.setOption(option, true);
+      },
     }
   }
 </script>
