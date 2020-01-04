0c3b3f15e6f1cf45176235d7c1c3e3b03da2ee07
何彬 <hebin@xinchao.com>
2020-01-04 19:40:27 +0800

    aa

--- src/views/putManage/list/modules/plan/List.vue
+++ src/views/putManage/list/modules/plan/List.vue
@@ -10,7 +10,9 @@
       <el-table v-loading="tableData.loading" :data="tableData.data" class="list_table">
         <el-table-column prop="name" label="投放计划名称">
           <template slot-scope="scope">
-            <span class="hand">{{scope.row.name}}</span>
+            <router-link :to="`/putManage?active=project&planId=${scope.row.id}`">
+              <span class="hand">{{scope.row.name}}</span>
+            </router-link>
           </template>
         </el-table-column>
 
