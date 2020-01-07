ce7a213564a943742f283a6225ad82596c52b1cf
liwen <XinChao@adx119>
2020-01-07 17:35:41 +0800

    地域分析数据mock添加

--- src/views/peopleInsight/peopleAnalysis/modeles/arealDistribution.vue
+++ src/views/peopleInsight/peopleAnalysis/modeles/arealDistribution.vue
@@ -6,7 +6,7 @@
       地域分析
     </div>
 
-    <div class="area-content">
+    <div class="area-content mid-between">
 
       <div class="content-left">
         <div class="choose-type">
@@ -18,16 +18,16 @@
         </div>
       </div>
 
-      <div class="content-right">
+      <div class="content-right" style="align-self: flex-start">
         <el-tabs  class="thin-tab small" v-model="activeTab">
           <el-tab-pane label="省份" name="province">
           </el-tab-pane>
           <el-tab-pane label="城市" name="city">
           </el-tab-pane>
           <ul class="map-list">
-            <li v-for="(item, index) in data[activeTab][mapType]" :key="index" class=" mid-between map-list-item">
-              <label class="t-name text-ellipsis">{{item.name}}</label>
-              <label class='t-value flex1'>
+            <li v-for="(item, index) in data[activeTab][mapType]" :key="index" class="mid-start map-list-item">
+              <label class="t-name text-ellipsis">{{index+1}}、{{activeTab === 'province'?item.displayName: item.name}}</label>
+              <label class='t-value'>
                 <div :style="{'width': item.countPercent}" class="value-content"></div>
               </label>
               <label v-if="activeTab === 'province'" class="value-text color-text-1">{{mapType === 0?$tools.toThousands(item.value,false): item.countPercent}}</label>
@@ -64,77 +64,83 @@ export default {
       activeTab: 'province',
       mapData: [],
       data: {},
+      options: {
+        grid: {
+          left: '0%',
+          right: '0%',
+        }
+      },
       fackProvinceTa:[
-        {'value': "0", 'name': "上海", 'countPercent': "0%"},
-        {'value': "0", 'name': "北京", 'countPercent': "0%"},
-        {'value': "0", 'name': "成都", 'countPercent': "0%"},
-        {'value': "0", 'name': "天津", 'countPercent': "0%"},
-        {'value': "0", 'name': "广东", 'countPercent': "0%"},
-        {'value': "0", 'name': "湖北", 'countPercent': "0%"},
-        {'value': "0", 'name': "浙江", 'countPercent': "0%"},
-        {'value': "0", 'name': "四川", 'countPercent': "0%"},
-        {'value': "0", 'name': "江苏", 'countPercent': "0%"},
-        {'value': "0", 'name': "重庆", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "上海市", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "北京市", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "重庆市", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "山西省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "浙江省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "安徽省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "山东省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "四川省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "湖南省", 'countPercent': "0%"},
+        {'value': "0", 'displayName': "天津市", 'countPercent': "0%"},
       ],
       fackCityTa:[
         {
           'value':[121.487899486,31.24916171,0],
           'label':'0',
-          'name':'上海',
+          'name':'上海市',
           'countPercent':'0%'
         },
         {
           'value':[116.395645038,39.9299857781,0],
           'label':'0',
-          'name':'北京',
+          'name':'北京市',
           'countPercent':'0%'
         },
         {
           'value':[104.067923463,30.6799428454,0],
           'label':'0',
-          'name':'成都',
+          'name':'成都市',
           'countPercent':'0%'
         },
         {
           'value':[117.210813092,39.1439299033,0],
           'label':'0',
-          'name':'天津',
+          'name':'天津市',
           'countPercent':'0%'
         },
         {
           'value':[113.307649675,23.1200491021,0],
           'label':'0',
-          'name':'广州',
+          'name':'广州市',
           'countPercent':'0%'
         },
         {
           'value':[114.025973657,22.5460535462,0],
           'label':'0',
-          'name':'深圳',
+          'name':'深圳市',
           'countPercent':'0%'
         },
         {
           'value':[118.778074408,32.0572355018,0],
           'label':'0',
-          'name':'南京',
+          'name':'南京市',
           'countPercent':'0%'
         },
         {
           'value':[120.219375416,30.2592444615,0],
           'label':'0',
-          'name':'杭州',
+          'name':'杭州市',
           'countPercent':'0%'
         },
         {
           'value':[114.316200103,30.5810841269,0],
           'label':'0',
-          'name':'武汉',
+          'name':'武汉市',
           'countPercent':'0%'
         },
         {
           'value':[106.530635013,29.5446061089,0],
           'label':'0',
-          'name':'重庆',
+          'name':'重庆市',
           'countPercent':'0%'
         }
       ],
@@ -143,13 +149,14 @@ export default {
   created() {
     this.data.province = this.normalizeProvinceData(this.areasList.provinceTa);
     this.data.city = this.normalizeCityData(this.areasList.cityTa);
-    console.log(this.data.province);
-    console.log(this.data.city);
   },
   mounted() {
     this.myChart = echarts.init(this.$refs.taMap); //这里是为了获得容器所在位置
-    window.onresize = this.myChart.resize;
+    window.addEventListener('resize', this.resize)
     this.reDrawMap()
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.resize)
   },
   watch: {
     mapType() {
@@ -160,6 +167,9 @@ export default {
     },
   },
   methods: {
+    resize() {
+      this.myChart.resize();
+    },
     reDrawMap() {
       let list = this.data[this.activeTab][this.mapType]
       let mapOptions = {}
@@ -306,12 +316,42 @@ export default {
       }
       this.myChart.setOption(mapOptions, true)
     },
+    getIndex(val, arr, type = 0) {
+      let index = arr.findIndex((item) => {
+        if (type === 0) {
+          return item.displayName == val.name
+        } else {
+          return item.name == val.name
+        }
+      })
+      return index
+    },
     normalizeProvinceData(arr) {
       // 0是ta人数的值 1是ta浓度的值
       let data = {0: [], 1: []}
       arr.forEach((item) => {
-        data[0].push({value: item.count, name: item.name.replace(/省/g,"").replace(/市/,""), countPercent: parseInt(item.countPercent, 10) * 100 + '%'})
-        data[1].push({value: (item.density * 100).toFixed(2), name: item.name.replace(/省/g,"").replace(/市/,""), countPercent: (item.density * 100).toFixed(2) + '%'})
+        let n = this.getIndex(item, this.fackProvinceTa)
+        if (n !== -1) {
+          this.fackProvinceTa.splice(n, 1)
+        }
+        data[0].push(
+          {
+          value: item.count,
+          displayName: item.name,
+          name: item.name.replace(/省/g,"").replace(/市/,""),
+          countPercent: parseInt(item.countPercent, 10) * 100 + '%'
+        },
+          ...this.fackProvinceTa.slice(0, 9)
+        )
+        data[1].push(
+          {
+          value: (item.density * 100).toFixed(2),
+          displayName: item.name,
+          name: item.name.replace(/省/g,"").replace(/市/,""),
+          countPercent: (item.density * 100).toFixed(2) + '%'
+          },
+          ...this.fackProvinceTa.slice(0, 9)
+        )
       })
       return data
     },
@@ -319,18 +359,24 @@ export default {
       // 0是ta人数的值 1是ta浓度的值
       let data = {0: [], 1: []}
       arr.forEach((item) => {
+        let n = this.getIndex(item, this.fackCityTa, 1)
+        if (n !== -1) {
+          this.fackCityTa.splice(n, 1)
+        }
         let val = [Number(item.lng), Number(item.lat)]
         data[0].push({
           value: val.concat(Number(item.count)),
           label: item.count,
-          name: item.name.replace(/省/g,"").replace(/市/,""),
+          displayName: item.name,
+          name: item.name,
           countPercent: parseInt(item.countPercent * 100, 10) + '%'
-        })
+        },  ...this.fackCityTa.slice(0, 9))
         data[1].push({
+          displayName: item.name,
           value: val.concat((item.density * 100).toFixed(2)),
-          name: item.name.replace(/省/g,"").replace(/市/,""),
+          name: item.name,
           countPercent: (item.density * 100).toFixed(2) + '%'
-        })
+        }, ...this.fackCityTa.slice(0, 9))
       })
       return data
     },
@@ -346,10 +392,11 @@ export default {
       color: $color-text;
     }
     .area-content{
-      display: flex;
+
       .content-left{
         position: relative;
-        width: 800px;
+        width: 50%;
+        /*width: 45%;*/
         .choose-type{
           position: absolute;
           top:0;
@@ -359,12 +406,20 @@ export default {
         .ta-people-map{
           position: relative;
           z-index: 1;
-          width:700px;
+          width: 100%;
+          max-width:700px;
           height:590px;
         }
       }
       .content-right{
-        flex-grow: 1;
+        width: 40%;
+        min-width: 530px;
+        & /deep/ .el-tabs.thin-tab.small .el-tabs__item {
+          margin-right: 50px;
+        }
+        & /deep/ .el-tabs.thin-tab.small .el-tabs__item.is-active {
+          border-bottom: 2px solid $color-main;
+        }
         .thin-tab{
           .map-list{
             margin-top: 10px;
