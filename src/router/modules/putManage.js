27316d1d07702b452a1a594941e3897343c164bf
chenxu <chenxu@xinchao.com>
2020-01-09 14:47:33 +0800

    '修改路由code'

--- src/router/modules/putManage.js
+++ src/router/modules/putManage.js
@@ -13,37 +13,37 @@ export const putManageRouter = [
   {
     path: '/putManage/create',
     component: () => import ('@/views/putManage/create/Index'),
-    meta: {code: '1310'},
+    meta: {code: '1310', name: '投放管理'},
     children: [
       //默认创建路径
       {
         path: '/',
         component: () => import ('@/views/putManage/create/modules/plan/Plan'),
-        meta: {code: '1310'},
+        meta: {code: '1310', name: '创建计划'},
       },
       // 创建计划
       {
         path: '/putManage/create/plan',
         component: () => import ('@/views/putManage/create/modules/plan/Plan'),
-        meta: {code: '1310'},
+        meta: {code: '1310', name: '创建计划'},
       },
       // 创建方案
       {
         path: '/putManage/create/project',
         component: () => import ('@/views/putManage/create/modules/project/Project'),
-        meta: {code: '1320'},
+        meta: {code: '1320', name: '创建方案'},
       },
       // 确认并支付
       {
         path: '/putManage/create/payConfirm',
         component: () => import ('@/views/putManage/create/modules/payConfirm/PayConfirm'),
-        meta: {code: '1330',white: true},
+        meta: {code: '1330', name: '确认并支付'},
       },
       // 创建创意
       {
         path: '/putManage/create/creative',
         component: () => import ('@/views/putManage/create/modules/creative/Creative'),
-        meta: {code: '1340',white: true},
+        meta: {code: '1340', name: '创建创意'},
       },
     ]
   }
--- src/router/modules/toolBox.js
+++ src/router/modules/toolBox.js
@@ -2,35 +2,18 @@
  * 工具箱
  */
 export const toolBoxRouter = [
-  // 工具箱--展示账号管理，辅助功能按钮
   {
-    path: '/toolBox',
-    component: () => import('@/views/toolBox/index'),
-  },
-  // 工具箱--账号管理信息
-  {
-    path: '/toolBox/account',
+    path: '/home/account',
     component: () => import('@/views/toolBox/manage/index'),
-    meta: {code: '1010'},
+    meta: {code: '1010',name:'账号管理信息'},
     children: [
       //默认账号管理路径---
       {
         path: '/',
         component: () => import('@/views/toolBox/manage/modules/account/Info'),
-        meta: {code: '1010'},
+        meta: {code: '1010',name:'账号管理信息'},
       },
     ]
-  },
-  // 资源包
-  // {
-  //   path: '/toolBox/resourceBundle',
-  //   component: () => import('@/views/toolBox/resourceBundle/'),
-  //   meta: {code: '1200'},
-  // },
-  // 工具箱--修改账号密码
-  {
-    path: '/toolBox/editPass',
-    component: () => import('@/views/toolBox/pass/index'),
   },
 ]
 
