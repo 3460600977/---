bbaf1077ab5fb6b5c55fb4fafdf3e7a2ed32e324
何彬 <hebin@xinchao.com>
2020-01-16 11:18:57 +0800

    邮件优化

--- src/views/mailCallback/MailCallback.vue
+++ src/views/mailCallback/MailCallback.vue
@@ -3,25 +3,32 @@
   <div v-loading="loading" element-loading-text="请稍候" class="mail-callback mid-center">
     <transition name="to-top">
       <div v-show="!loading" class="mail-callback-box">
-        <div class="title center font-20 bold">
-          <template v-if="callbackData.success">支付成功</template>
-          <template v-if="!callbackData.success">支付失败</template>
+        <div class="title font-14 bold mid-center">
+          <template v-if="callbackData.success">
+            <i class="iconfont icon--zhengque-shiti color-main"></i>
+            支付成功
+          </template>
+          <template v-if="!callbackData.success">
+            <i class="iconfont icon-error color-main"></i>
+            支付失败
+          </template>
         </div>
 
-        <el-divider></el-divider>
 
         <div class="message">
           <!-- 成功 -->
           <template v-if="callbackData.success">
             <div class="success-box">
-              <p class="account">支付金额: ¥ <span class="font-number">{{$tools.toThousands(callbackData.data.account / 100)}}</span></p>
-              <p class="balance">账户余额: ¥ <span class="font-number">{{$tools.toThousands(callbackData.data.balance / 100)}}</span></p>
+              <p class="account">支付金额：<span class="color-main">¥ <span class="font-number font-18">{{$tools.toThousands(callbackData.data.account / 100)}}</span>
+                </span>
+              </p>
+              <p class="balance">账户余额：¥ <span class="font-number font-18">{{$tools.toThousands(callbackData.data.balance / 100)}}</span></p>
             </div>
           </template>
 
           <!-- 失败 -->
           <template v-if="!callbackData.success">
-            <div class="fail-box text-center">
+            <div class="message-box color-text-1">
               {{callbackData.msg}}
             </div>
           </template>
@@ -38,7 +45,7 @@ export default {
       loading: true,
 
       callbackData: {
-        success: false,
+        success: true,
         data: null,
         msg: null
       }
@@ -78,16 +85,35 @@ export default {
   .mail-callback{
     height: 100%;
     width: 100%;
+    background-image: url('../../assets/images/mail_bg.png');
+    background-size: cover;
+    background-position: center;
     .mail-callback-box{
-      border-radius: 2px;
-      box-shadow: 0 0 40px rgba(0, 0, 0, 0.2);
-      padding: 30px;
-      width: 500px;
+      position: relative;
+      box-shadow:0px 8px 16px 2px rgba(67,3,3,0.1);
+      padding: 72px 0 0;
+      width:568px;
+      height:280px;
+      background-color: #fff;
+      >.title{
+        >.iconfont{
+          font-size: 30px;
+          margin-right: 20px;
+        }
+      }
       >.message{
-        padding: 30px 70px 50px;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        margin-top: 60px;
+        .message-box{
+          position: relative;
+          color: $color-table-title;
+          line-height: 20px;
+        }
         .success-box{
           .account,.balance{
-            line-height: 20px;
+            margin-bottom: 20px;
           }
         }
       }
